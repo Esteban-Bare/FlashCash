520358bce5fc5b402594f9ae4b78a05b973674e6
Esteban <150131639+Esteban-Bare@users.noreply.github.com>
2024-12-20 14:18:17 +0100

    Commit all

---
+++ src/main/java/com/example/flascash/entities/Transaction.java
@@ -0,0 +1,98 @@
+package com.example.flascash.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_user_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_user_id")
+    private User receiver;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Column(nullable = true)
+    private String fee;
+
+    @Column(nullable = true)
+    private String description;
+
+    public Transaction() {}
+
+    public Transaction(User sender, User receiver, Double amount, LocalDateTime date, String description) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+        this.date = date;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getFee() {
+        return fee;
+    }
+
+    public void setFee(String fee) {
+        this.fee = fee;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/example/flascash/service/AccountService.java
@@ -0,0 +1,37 @@
+package com.example.flascash.service;
+
+import com.example.flascash.entities.Account;
+import com.example.flascash.entities.User;
+import com.example.flascash.repositories.AccountRepository;
+import com.example.flascash.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private SessionService sessionService;
+
+    public Account saveAccount(String iban, User user) {
+        if (accountRepository.findByIban(iban).isPresent()) {
+            throw new IllegalArgumentException("account already exists");
+        }
+
+        Account account = new Account(user,iban, 0.0);
+        accountRepository.save(account);
+        return account;
+    }
+
+    public Account findById(Long id) {
+        return accountRepository.findById(id).get();
+    }
+
+    public void addMoney(double money, Account account) {
+        account.setBalance(account.getBalance() + money);
+        accountRepository.save(account);
+    }
+}
